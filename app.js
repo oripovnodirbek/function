73294ed5b5d28d71210dd34f3f7b42f5efa9b102
oripovnodirbek <nodirbekoripov1333@gmail.com>
2024-04-27 02:25:08 +0500

    5 dars

---
+++ app.js
@@ -0,0 +1,214 @@
+// let num1 = +prompt('birinchi sonni kiritng')
+// let num2 = +prompt('ikkinchi sonni kiritng')
+// function sum(x, y){
+//     result=x + y
+//     console.log(result)
+// }
+// sum(num1, num2)
+
+
+// let minut = +prompt('Daqiqani kiriting')
+// let second = 60
+// function sum(x, y){
+//     result=x*y
+//     console.log(result)
+// }
+// sum(minut, second)
+
+
+// let num = +prompt('Rqaqmni kiriting')
+// let whan = 1
+// function sum(x, y){
+//     result=x+y
+//     console.log(result)
+// }
+// sum(num, whan)
+
+
+// let a = +prompt('uchburchakni boyini kiritng')
+// let b = +prompt('Uchburchakni asosini kiriting')
+// function sum(x, y) {
+//     result=(x*y)/2
+//     console.log(result)
+// }
+// sum(a, b)
+
+
+// let leater = +prompt('Yilni kiriting')
+// let yil = 365
+// function sum(x, y){
+//     result=x*y
+//     console.log(result)
+// }
+// sum(leater, yil)
+
+
+// let num = +prompt('sonni kiriting')
+// let a = 3
+// function sum(x, y){
+//     result = x**y
+//     console.log(result)
+// }
+// sum(num, a)
+
+
+// let num1 = +prompt('sonni kiriting')
+// let num2 = +prompt('sonni kiriting')
+// let num3 = +prompt('sonni kiriting')
+// function sum(x, y, z){
+//     result = x
+//     console.log(result)
+// }
+// sum(num1, num2, num3)
+
+
+// let num1 = +prompt('birinchi sonni kiriting')
+// let num2 = +prompt('ikkinchi sonni kiriting')
+// function sum(x, y){
+//     result=x*y
+//     console.log(result)
+// }
+// sum(num1, num2)
+
+
+// let num = +prompt('Soatni kiriting')
+// let minut = 60*60
+// function sum(x, y){
+//     result=x*y
+//     console.log(result)
+// }
+// sum(num, minut)
+
+
+// let a = +prompt('uchburchak tomonini kiriting')
+// let b = +prompt('ikkinchi tomonni kiriting')
+// function sum(x, y){
+//     result=(x+y)-1
+//     console.log(result)
+// }
+// sum(a, b)
+
+
+// let a = +prompt('son1')
+// let b = +prompt('son2')
+// function sum(x, y){
+//     result = x%y
+//     console.log(result)
+// }
+// sum(a, b)
+
+
+// let boy = +prompt('tortburchakni boyini kiriting')
+// let en = +prompt('endi esa enini')
+// function sum (x, y){
+//     result = (x+y)*2
+//     console.log(result)
+// }
+// sum(boy, en)
+
+
+// let a = +prompt('sonni kiriting')
+// function sum(x, y = x){
+//     result = x*y
+//     console.log(result)
+// }
+// sum(a)
+
+
+// let a = +prompt('sonni kiriting')
+// function sum(x){
+//     if(x > 0){
+//         retur = false;
+//         console.log(retur)
+//     } else if(x <= 0) {
+//         reture = true;
+//         console.log(reture)
+//     }
+// }
+// sum(a)
+
+
+// let a = +prompt('burchaklar sonini kiriting')
+// function sum(x){
+//     result = (x - 2)*180
+//     console.log(result)
+// }
+// sum(a)
+
+
+// let shot1 = +prompt('1-sonni kiriting')
+// let shot2 = +prompt('2-sonni kiriting')
+// function sum(x, y){
+//     result = (x*2)+(y*3)
+//     console.log(result)
+// }
+// sum(shot1, shot2)
+
+
+// let a = +prompt("birinchi sonni kiriting")
+// let b = +prompt('ikkinchi sonni kiriting')
+// c = a + b
+// function sum(x){
+//     if(x <= 100){
+//         result = true
+//     } else if(x > 100) {
+//         result = false
+//     }
+//     console.log(result)
+// }
+// sum(c)
+
+
+// let a = +prompt('sonni kiriting')
+// function sum(x){
+//     let result = []
+//     for(let i=1; i<=x; i++){
+//         result.push(i)
+//     }
+//     console.log(result)
+// }
+// sum(a)
+
+
+// let num1 = +prompt('1-sonni kiriting')
+// let num2 = +prompt('2-sonni kiriting')
+// function sum(x, y){
+//     if(x==y){
+//         result = true
+//     }else if(x>y){
+//         result = false
+//     }else if(x<y){
+//         result = false
+//     }
+//     console.log(result)
+// }
+// sum(num1, num2)
+
+
+// let yutug = +prompt('yutugni kiriting')
+// let durang = +prompt('durangni kiriting')
+// let maglub = +prompt('maglubiyatni kiriting')
+// function sum(x, y, z){
+//     x = x*3
+//     z = z*0
+//     result = (x + y) + z
+//     console.log(result)
+// }
+// sum(yutug, durang, maglub)
+
+
+// let a = +prompt('qiymatni kiriting')
+// let b = +prompt('qiymatni kiriting')
+// function sum(x, y){
+//     if(x === y){
+//         result = true
+//     }else {
+//         result = false
+//     }
+//     console.log(result)
+// }
+// sum(a, b)
+
+
+// const arrowFunc = () => prompt("Qiymatni kiriting:");
+// console.log(arrowFunc());
